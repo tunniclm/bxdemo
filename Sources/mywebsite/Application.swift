7f9215e99e77f2e2aa822101caa45575b52b9ce5
Mike Tunnicliffe <mike.tunnicliffe@uk.ibm.com>
2017-02-17 22:38:19 +0000

    resiliance

--- Sources/mywebsite/Application.swift
+++ Sources/mywebsite/Application.swift
@@ -30,8 +30,12 @@ public func initialize() throws {
         print(error)
     }
     // Set up monitoring
+    do {
     let sm = try SwiftMetrics()
     let _ = try SwiftMetricsDash(swiftMetricsInstance : sm, endpoint: router)
+    } catch {
+        print(error)
+    }
 
 
     // Configuring cloudant
